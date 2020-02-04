cdc09402d7ee965c654ac2dcabb8dee9b89b8ea1
Shunk <Matthew.Shunk@usfoods.com>
2020-02-04 11:34:29 -0600

    updated documentation

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # compare_text
 compare two texts and determine their similarity between 0 and 1
+
+usage script arg1 arg2
+
+arg1 is text1 arg2 is text2
--- compare_text.py
+++ compare_text.py
@@ -9,56 +9,56 @@
 
 import sys
 try:
-    old_file = sys.argv[1]
-    new_file = sys.argv[2]
+    file1 = sys.argv[1]
+    file2 = sys.argv[2]
 except:
     print("must provide the path of two files as arguments")
     quit()
 #print ('list: ' + str(sys.argv))
 
 
-def compare(old,new):
-    old_map = dict()
+def compare(t1,t2):
+    t1_map = dict()
     #set up a dict of each value and it's corresponding coordinates. so the string 
     #'hello' would return something like h:0 e:1 l:2,3 o:4
-    for i, val in enumerate(old):
+    for i, val in enumerate(t1):
         #print('i: ' + str(i))
         #print('val: ' + str(val))
-        old_map.setdefault(val,list()).append(i)
+        t1_map.setdefault(val,list()).append(i)
 
     match = dict()
-    #index of start of largest overlap in on old
-    sold = 0
-    #index of beginning of same substring in new list
-    snew = 0
+    #index of start of largest overlap in on t1
+    sub_st_t1 = 0
+    #index of beginning of same substring in t2 list
+    sub_st_t2 = 0
     #length of overlap
     length = 0
 
-    #inew is the index of each letter and val is the value
-    for inew, val in enumerate(new):
-        #newmatch used for holding 
-        newmatch = dict()
-        for iold in old_map.get(val,list()):
-            #newmatch at index of iold is set to match + 1 only if iold and match.get are non 0
-            newmatch[iold] = (iold and match.get(iold - 1, 0)) + 1
+    #it2 is the index of each letter and val is the value
+    for it2, val in enumerate(t2):
+        #t2match used for ht1ing 
+        t2match = dict()
+        for it1 in t1_map.get(val,list()):
+            #t2match at index of it1 is set to match + 1 only if it1 and match.get are non 0
+            t2match[it1] = (it1 and match.get(it1 - 1, 0)) + 1
             #set values of length and match starts
-            if(newmatch[iold] > length):
-                length = newmatch[iold]
-                sold = iold - length + 1
-                snew = inew - length + 1
-        match = newmatch
+            if(t2match[it1] > length):
+                length = t2match[it1]
+                sub_st_t1 = it1 - length + 1
+                sub_st_t2 = it2 - length + 1
+        match = t2match
 
 
     if length == 0:
         #no common substring returns insert and delete
-        return (old and [('-', old)] or []) + (new and [('+', new)] or [])
+        return (t1 and [('-', t1)] or []) + (t2 and [('+', t2)] or [])
     else:
         #recursively compare the text before and after the common substring
         #will produce complete list of all matches inserts and deletes
-        return compare(old[ : sold], new[ : snew]) + \
-                [('=', new[snew : snew + length])] + \
-                compare(old[sold + length : ],
-                        new[snew + length : ])
+        return compare(t1[ : sub_st_t1], t2[ : sub_st_t2]) + \
+                [('=', t2[sub_st_t2 : sub_st_t2 + length])] + \
+                compare(t1[sub_st_t1 + length : ],
+                        t2[sub_st_t2 + length : ])
         
 #text1="""The easiest way to earn points with Fetch Rewards is to just shop for the best products you already love. If you have any participating brands on your receipt, you'll get points based on the cost of the products. You don't need to clip any coupons or scan individual barcodes. Just scan each grocery receipt after you shop and we'll find the savings for you."""
 #text2="""The easiest way to earn points with Fetch Rewards is to just shop for the items you already buy. If you have any eligible brands on your receipt, you will get points based on the total cost of the products. You do not need to cut out any coupons or scan individual UPCs. Just scan your receipt after you check out and we will find the savings for you."""
@@ -74,9 +74,9 @@ def compare(old,new):
 
 #open and read file
 try:
-    f1 = open(old_file)
+    f1 = open(file1)
     contents1=f1.read()
-    f2 = open(new_file)
+    f2 = open(file2)
     contents2=f2.read()
 except:
     print("cannot open files")
